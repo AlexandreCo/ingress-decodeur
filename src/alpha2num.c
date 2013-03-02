a3e567bb7a0f3ff38afdf27bae469449e1353b9b
gnome <gnome@gnome-tiny.(none)>
2013-03-03 00:03:38 +0100

    add reverse and alphanum decoder

---
+++ src/alpha2num.c
@@ -0,0 +1,50 @@
+/*
+ ============================================================================
+ Name        : decode.c
+ Author      : Chaf0uin
+ Version     :
+ Copyright   : Your copyright notice
+ Description : decode in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "libdecode.h"
+typedef enum { false = 0, true = 1 } bool;
+
+
+int main(int argc, char *argv[])  {
+
+	char * pcPosIn;
+	char * pcPosOut;
+	char acDeCode[255];
+	int iIndex=0;
+	int i;
+	char acCode[255];
+
+	bzero(acCode,255);
+	printf("[2-9][p-z][a-h][2-9]keyword[p-z][2-9][p-z][2-9][p-z]\n");
+
+	for(pcPosOut=acCode,iIndex=0;iIndex<argc-1;iIndex++)
+	{
+		iGet(argv[iIndex+1],&pcPosOut);
+		iAddPad(&pcPosOut);
+	}
+
+	for(iIndex=0;iIndex<strlen(acCode);iIndex++)
+	{
+		acDeCode[iIndex]=acCode[iIndex];
+		if((acCode[iIndex]<='i')&&(acCode[iIndex]>='a'))
+		{
+			acDeCode[iIndex]='0'+acCode[iIndex]-'a'+1;
+		}
+	}
+
+
+	printf("%s\n", acDeCode);
+	return EXIT_SUCCESS;
+}
+
+
